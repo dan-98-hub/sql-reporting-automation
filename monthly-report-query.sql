bba5e5e152895b380e29a81897589ed12462f122
Daniela Salinas <d.alexandra.se@gmail.com>
2026-05-18 13:08:23 -0500

    Update monthly-report-query.sql

--- monthly-report-query.sql
+++ monthly-report-query.sql
@@ -1,4 +1,4 @@
 SELECT department, COUNT(*) AS total_appointments
 FROM appointments
-WHERE appointment_date BETWEEN '2026-01-01' AND '2026-01-31'
+WHERE appointment_date BETWEEN '2020-01-01' AND '2021-01-31'
 GROUP BY department;
